0cd3c808468404099239143bf82a89d89f270b3a
tomyferra <ferra.tomy@gmail.com>
2024-05-09 16:59:30 -0300

    Modal

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import React from "react";
 import Image from "next/image";
 import FioreyTomi from "../images/FioreyTomiBlack.png";
 import IconoAnillos from '../images/icono-anillos.svg'
@@ -10,13 +11,20 @@ import Img2 from '../images/img2.webp'
 import Img3 from '../images/img3.webp'
 import Img4 from '../images/img4.webp'
 import CountUp from 'react-countup';
+import {
+  Dialog,
+} from "@material-tailwind/react";
 
 
 export default function Home() {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(!open);
   var hoy = new Date();
   var aniversario = new Date('2012-02-04');
   var diferencia = hoy - aniversario;
   var diferenciaDias = diferencia / (1000 * 60 * 60 * 24);
+
 
   return (
     <>
@@ -140,9 +148,24 @@ export default function Home() {
           width={150}
         />
         <p className="text-l text-center">Tu presencia es el mejor regalo!</p>
-        <p className="text-l text-center">Pero si querés, podés colaborar con nuestra</p>
-        <p className="text-l text-center p-2">Luna de Miel...</p>
-        <a target="_blank" href="https://maps.app.goo.gl/qyEwKpHfan6BHgN98" className="p-2 m-2 bg-white border hover:bg-transparent text-[#a6bcc8] hover:text-white py-2 px-4 rounded-full">Ver Datos Bancarios</a>
+        <p className="text-l text-center p-2">Pero si querés, podés colaborar con nuestra Luna de Miel!</p>
+        <button onClick={handleOpen} className="p-2 m-2 bg-white border hover:bg-transparent text-[#a6bcc8] hover:text-white py-2 px-4 rounded-full">Ver Datos Bancarios</button>
+
+        <Dialog open={open} handler={handleOpen} className="fixed overflow-auto inset-0 z-50 m-0">
+          <div className="grid place-items-center w-screen h-screen bg-black bg-opacity-50">
+          <div className="relative rounded-lg text-blue-gray-500 antialiased font-sans text-base font-light leading-relaxed w-full md:w-3/5 lg:w-2/5 2xl:w-1/4 min-w-[80%] md:min-w-[60%] lg:min-w-[40%] 2xl:min-w-[25%] max-w-[80%] md:max-w-[60%] lg:max-w-[40%] 2xl:max-w-[25%] bg-transparent shadow-none">
+            <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md mx-auto w-full max-w-[24rem]">
+            <h1 className="text-xl text-center font-bold">Datos Bancarios</h1>
+            <p className="text-l text-center">Nombre del Titular: Tomas Ferrari</p>
+            <p className="text-l text-center">CBU: 0170099240000062957416</p>
+            <p className="text-l text-center">Alias: ferrari.tomas</p>
+            <p className="text-l text-center">DNI: 38258853</p>
+            <p className="text-l text-center">Banco BBVA</p>
+            <button onClick={handleOpen} className="p-2 m-2 border secondaryColor hover:bg-white hover:text-black text-white py-2 px-4 rounded-full">Cerrar</button>
+            </div>
+            </div>
+          </div>
+        </Dialog>
       </div>
       {/* ################################################################################################
       ################################################################################################ */}

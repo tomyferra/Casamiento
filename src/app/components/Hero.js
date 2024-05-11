ecff0359cfd5675fc9cb9787da1672593f754890
tomyferra <ferra.tomy@gmail.com>
2024-05-11 15:08:28 -0300

    Fixed bg image

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -5,7 +5,7 @@ import FioreyTomi from "../../images/FioreyTomiBlack.png";
 
 function Hero() {
   return (
-    <div className="h-100 flex flex-col bg-cover bg-fixed bg-center bg-no-repeat shadow-lg backgroundImage">
+    <div className="flex flex-col bg-cover bg-center shadow-lg backgroundImage">
     <div className="p-4 sm:p-8 flex items-center justify-center">
       <Image src={FioreyTomi} alt="logoCasamiento" />
       <a href="#Detalles"><div className="scroll-down"></div></a>

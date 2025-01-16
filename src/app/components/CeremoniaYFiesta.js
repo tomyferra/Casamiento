3caeb25b90de7669695cd4b0568505615f0e4a6a
tomyferra <ferra.tomy@gmail.com>
2025-01-16 17:09:43 -0300

    changed direction

--- src/app/components/CeremoniaYFiesta.js
+++ src/app/components/CeremoniaYFiesta.js
@@ -14,7 +14,7 @@ function CeremoniaYFiesta() {
       <p className="text-l text-center">Sábado 15 de Marzo de 2025 a las 17:30 Hs</p>
       <p className="text-l text-center">Espacio Mendoza, El Molino</p>
       <p className="text-l text-center p-2">¡Te esperamos!</p>
-      <a target="_blank" href="https://maps.app.goo.gl/2iSsveYe3gT2kWkM9" className="p-2 m-2 border bg-white hover:bg-transparent hover:text-black text-black py-2 px-4 rounded-full">COMO LLEGAR</a>
+      <a target="_blank" href="https://maps.app.goo.gl/7eVSF4kUFJHfHDy78" className="p-2 m-2 border bg-white hover:bg-transparent hover:text-black text-black py-2 px-4 rounded-full">COMO LLEGAR</a>
     </div>
   )
 }
